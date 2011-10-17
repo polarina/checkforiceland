9c5f10881a104eb9880307b9ab6fbb26d330fe4e
Gabríel A. Pétursson <gabriel@system.is>
2011-10-17 00:49:04 +0000

    Initial commit

---
+++ checkforiceland.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = "checkforiceland"
+  s.version     = "0.0.0"
+  s.date        = "2011-10-16"
+  s.summary     = "Check for Iceland"
+  s.description = ""
+  s.authors     = ["Anton Smári Gunnarsson", "Birkir Guðjónsson", "Gabríel A. Pétursson", "Gunnar Guðvarðarson"]
+  s.email       = "gabriel@system.is"
+  s.files       = ["lib/checkforiceland.rb"]
+  s.homepage    = "http://rubygems.org/gems/hola"
+end
---
+++ lib/checkforiceland.rb
@@ -0,0 +1,19 @@
+class CheckForIceland
+  def is_icelandic?(address)
+    address = IPAddr.new(address) unless address.class == IPAddr
+    address = address.native
+    
+    Socket.getaddrinfo address.reverse.gsub("in-addr.arpa", "iceland.rix.is"), nil if address.ipv4?
+    Socket.getaddrinfo address.reverse.gsub("ip6.arpa",     "iceland.rix.is"), nil if address.ipv6?
+    
+    true
+  rescue SocketError => e
+    if e.to_s == "getaddrinfo: Name or service not known"
+      false
+    elsif e.to_s == "getaddrinfo: Temporary failure in name resolution"
+      nil
+    else
+      raise e
+    end
+  end
+end
